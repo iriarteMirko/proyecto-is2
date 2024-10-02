f089c267e740524bf77222ff5b7bea8e7e84bc61
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-10-01 21:33:05 -0500

    Add apps sprint1

--- reserva_tu_cancha/urls.py
+++ reserva_tu_cancha/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.usuario.urls')),
+    path('usuario/', include('apps.usuario.urls')),
+    path('', include('apps.cliente.urls')),
+    path('cliente/', include('apps.cliente.urls')),
+    path('', include('apps.responsable.urls')),
+    path('responsable/', include('apps.responsable.urls')),
+    path('', include('apps.cancha.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Configuración de la vista personalizada para errores 404
+handler404 = 'apps.usuario.views.error_404_view'
