47e80cf4cf7cfed6d3b26c005dafbd2fd376d2ea
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-10-12 13:51:56 -0500

    Update x

--- apps/cancha/signals.py
+++ apps/cancha/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from .models import Cancha
