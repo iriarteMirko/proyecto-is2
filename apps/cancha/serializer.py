c46a54d5f0befb884720e3cbaf7e05cec74148f9
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-11-10 14:04:40 -0500

    Update cancha imagen

--- apps/cancha/serializer.py
+++ apps/cancha/serializer.py
@@ -4,5 +4,5 @@ from .models import Cancha
 class CanchaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cancha
-        fields = ['id', 'nombre', 'fecha_creacion', 'ultima_modificacion', 'responsable']
-        read_only_fields = ['disponibilidad', 'fecha_creacion', 'ultima_modificacion']
+        fields = ['id', 'nombre', 'fecha_creacion', 'ultima_modificacion', 'responsable', 'imagen', 'slug']
+        read_only_fields = ['disponibilidad', 'fecha_creacion', 'ultima_modificacion', 'slug']
